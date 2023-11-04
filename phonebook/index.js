c8a102e6bf694c09bb3d9bbc15d332ca40a59b81
benjamin ws <56019855+bwongsee@users.noreply.github.com>
2023-11-04 17:35:27 +1100

    Update index.js

--- phonebook/index.js
+++ phonebook/index.js
@@ -9,33 +9,62 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.static('build'))
 
+//rest get
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(result => {
     res.json(result)
   })
 })
 
-app.get('/info', (req, res) => {
+//rest info page
+app.get('/info', (req, res, next) => {
   const date = new Date()
   console.log(date)
-  res.send(
-    `<p>Phonebook has info for ${persons.length} persons</p>
-    <br>
-    <p>${date}</p>`
-  )
-})
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    response.json(person)
+  Person.find({}).then(result => {
+    res.send(
+      `<p> Phonebook has info for ${res.length} people</p>
+      <br>
+      <p>${date}</p>`
+    )
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.filter(persons => persons.id !== id)
+//rest get by id
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
+    if(person) {
+      res.json(person)
+    }
+    else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
 
-  res.status(204).end()
+//rest delete
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id).then(result => {
+    res.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+//rest put
+app.put('api/persons/:id',(req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new:true}).then(updatedPerson => {
+    res.json(updatedPerson)
+  })
+  .catch(error => next(error))
 })
 
 const postMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -54,8 +83,27 @@ app.post('/api/persons', postMorgan, (req, res) => {
     person.save().then(personToAdd => {
       response.json(personToAdd)
     })
+    .catch (error => next(error))
   }
 })
+
+//handler of requests with unkown endpoint
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+//handler of requests with result to errors
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  
+  if (error.name === 'CastError') {
+    return res.status(400).send({error: 'malformatted id'})
+  }
+
+  next (error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
